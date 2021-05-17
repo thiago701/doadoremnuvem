be07339294d4437ae04d299e8389b1db65a7491d
ayrtondouglas <ayrtondouglas@users.noreply.github.com>
2021-05-17 01:52:42 -0300

    Correções parciais de bugs da sprint2 e implementadas algumas melhorias

--- doadornuvem-frontend/doadoremnuvem/src/app/usuarios/usuarios.component.ts
+++ doadornuvem-frontend/doadoremnuvem/src/app/usuarios/usuarios.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
 import { data } from 'jquery';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { Usuario } from '../login/usuario';
 import { UsuarioService } from './usuario.service';
 
@@ -15,28 +16,62 @@ export class UsuariosComponent implements OnInit {
   public tipoCadastro: any;
   public usuarios: Array<Usuario>;
 
+  formCadastro :FormGroup;
+
   constructor(
     private usuarioService: UsuarioService,
-  
+    private confimationService : ConfirmationService,
+    private messageService : MessageService,
+    private formBuilder :FormBuilder
     ) { }
 
   ngOnInit(): void {
     // Seta uma classe no BODY para controle de interface
     const body = document.getElementsByTagName('body')[0];
     body.classList.add('page-usuarios');
-
+    
+    this.formCadastro = this.formBuilder.group({
+      nome:[null, Validators.required, Validators.minLength(3)],
+      email:[null,[Validators.required, Validators.email]],
+      documento:[null] 
+    });
   }
   onSubmit() {
-    console.log(this.usuario);
-    this.usuarioService.salvarUsuario(this.usuario).subscribe(
-      (data) => {
-      },
-      (err) => {
-        console.log(err);
-      }   
-    );
-    this.getUsuarios();
+    // if (this.formCadastro.valid){
+      this.usuarioService.salvarUsuario(this.usuario).subscribe(
+        (data) => {
+         console.log("dados"+data[1])
+          this.messageService.add({severity:'success', summary:'Operação realizada', detail:'Usuário atualizado com sucesso!'})
+          this.getUsuarios();
+        },
+        (err) => {
+          
+          if (err.status == 400){
+             this.messageService.add({severity:'error', summary:'Operação não realizada', detail:'Já existe um usuário cadastrado com este cpf!'})
+          }else if (err.status == 500){
+            this.messageService.add({severity:'error', summary:'Operação não realizada', detail:'Falha ao cadastrar o usuário. Parâmetros inválidos'})
+          }else{
+            this.messageService.add({severity:'error', summary:'Operação não realizada', detail:'Falha ao cadastrar o usuário:' + err.string}) 
+          }
+        }   
+      );
+    // }else{
+    //   console.log('Formalário inválid');
+    //  Object.keys(this.formCadastro.controls).forEach(campo => {
+    //    console.log(campo);
+
+    //   //  const controle this.formCadastro.get()get        
+    //  });      
+    // }
   }
+
+  cancelarCadastro(){
+    this.usuarioService.listarUsuarios().subscribe(
+      (data) =>{
+        this.messageService.add({severity:'error', summary:'Operação não realizada', detail:'Cadastro cancelado!'})
+      } ); 
+  }
+
 
   getUsuarios() {
     this.usuarioService.listarUsuarios().subscribe(
@@ -47,5 +82,15 @@ export class UsuariosComponent implements OnInit {
         console.log(err);
       }
     );
+  }
+  verificaValidTouched(campo){
+    return !this.formCadastro.get(campo).valid && this.formCadastro.get(campo).touched;
+  }
+  aplicaCssErro(campo){
+    return{
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo)
+
+    }
   }
 }
